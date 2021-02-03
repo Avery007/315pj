c9061c893b9104a8f5554515452a2d356e7c62ee
Avery <44121063+Avery007@users.noreply.github.com>
2021-02-03 09:39:02 -0500

    Ex2
    updates for exercise2: added rotation

---
+++ Assets/basicScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicScript : MonoBehaviour
+{
+    public Vector3 rotation = new Vector3(0, 3.0f, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetKey(KeyCode.R))
+        {
+            this.GetComponent<Transform>().Rotate(rotation);
+        }
+
+    }
+}
