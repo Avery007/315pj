807a786fa1fa13e473fc0b0af57b093c9c7cc5f4
Avery <44121063+Avery007@users.noreply.github.com>
2021-02-09 15:41:13 -0500

    added more scripts

--- Assets/pickup.cs
+++ Assets/pickup.cs
@@ -14,6 +14,7 @@ public class pickup : MonoBehaviour
             Debug.Log("collision pickup");
             Destroy(collision.collider.gameObject);
         }
+    }
         void Start()
     {
         
