58128495677f7c85abd349feec9c9dfc3f42f176
Avery <44121063+Avery007@users.noreply.github.com>
2021-02-09 15:26:42 -0500

    added pickup

---
+++ Assets/pickup.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string pickUpTag;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag(pickUpTag))
+        {
+            Debug.Log("collision pickup");
+            Destroy(collision.collider.gameObject);
+        }
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
